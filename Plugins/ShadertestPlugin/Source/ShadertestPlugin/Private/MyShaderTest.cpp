28b2767593706a80ab41ae828887b479c8cb4a05
G2Z74T3-VCV\Admin <1255403211@qq.com>
2024-04-02 21:44:45 +0800

    --init: add texture Shader

--- Plugins/ShadertestPlugin/Source/ShadertestPlugin/Private/MyShaderTest.cpp
+++ Plugins/ShadertestPlugin/Source/ShadertestPlugin/Private/MyShaderTest.cpp
@@ -41,22 +41,13 @@ public:
  
     }
 
-    static bool ShouldCache(EShaderPlatform Platform)  
-    {  
-        return true;  
-    }  
- 
+    
     static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)  
     {  
-        return IsFeatureLevelSupported(Parameters.Platform, ERHIFeatureLevel::SM5);  
         return true;  
     }
  
-    static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment)  
-    {  
-        FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);  
-        OutEnvironment.SetDefine(TEXT("TEST_MICRO"), 1);  
-    }  
+    
 };  
 
 BEGIN_GLOBAL_SHADER_PARAMETER_STRUCT(FMyUniformStructData, )
@@ -73,52 +64,38 @@ class FShaderTestPS : public FGlobalShader
 public:  
     FShaderTestPS() {}  
 
-
-
-
     FShaderTestPS(const ShaderMetaType::CompiledShaderInitializerType& Initializer)  
         : FGlobalShader(Initializer)  
     {  
-       // SimpleColorVal.Bind(Initializer.ParameterMap, TEXT("SimpleColor"));
-       //  UE_LOG(LogTemp, Error, TEXT("harylv: error"));
+        Color.Bind(Initializer.ParameterMap, TEXT("MyColor"));
+        ColorScale.Bind(Initializer.ParameterMap, TEXT("MyColorScale"));
+        Texture.Bind(Initializer.ParameterMap, TEXT("InTexture"));
+        TextureSampler.Bind(Initializer.ParameterMap, TEXT("InTextureSampler"));
     }
-
-   
-
-    static bool ShouldCache(EShaderPlatform Platform)  
-    {  
-        return true;  
-    }  
- 
+    
     static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)  
     {  
         return IsFeatureLevelSupported(Parameters.Platform, ERHIFeatureLevel::SM5);  
     }
- 
-    static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment)  
-    {  
-        FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);  
-        OutEnvironment.SetDefine(TEXT("TEST_MICRO"), 1);  
-    }
-
-   
     
     void SetParameters(  
        FRHICommandListImmediate& RHICmdList,  
-       const FVector4& MyColor  
+       const FTexture* TextureValue, const FLinearColor& InColor, float InColorScale 
        )  
     {
         // FShaderTestPS::FParameters* PassParameters = GraphBuilder.AllocParameters<FShaderTestPS::FParameters>();
         // PassParameters->SimpleColor = MyColor;
         FRHIPixelShader* PS = RHICmdList.GetBoundPixelShader();
-        // SetShaderValue(RHICmdList, PS, SimpleColorVal, MyColor);
-        FMyUniformStructData ShaderData;
-        ShaderData.SimpleColor = MyColor;
-        SetUniformBufferParameterImmediate(RHICmdList, PS, GetUniformBufferParameter<FMyUniformStructData>(), ShaderData);
+        SetTextureParameter(RHICmdList, PS, Texture, TextureSampler, TextureValue);
+        SetShaderValue(RHICmdList, PS, Color, InColor);
+        SetShaderValue(RHICmdList, PS, ColorScale, InColorScale);
     }
 
 private:
-    
+    LAYOUT_FIELD(FShaderParameter, Color);
+    LAYOUT_FIELD(FShaderParameter, ColorScale);
+    LAYOUT_FIELD(FShaderResourceParameter, Texture);
+    LAYOUT_FIELD(FShaderResourceParameter, TextureSampler);
     // FShaderParameter SimpleColorVal;  
 
 };
@@ -128,102 +105,112 @@ private:
 
 IMPLEMENT_SHADER_TYPE(, FShaderTestVS, TEXT("/Plugin/ShadertestPlugin/Private/MyShader.usf"), TEXT("MainVS"), SF_Vertex)  
 IMPLEMENT_SHADER_TYPE(, FShaderTestPS, TEXT("/Plugin/ShadertestPlugin/Private/MyShader.usf"), TEXT("MainPS"), SF_Pixel)  
+
+struct FCustomVertex
+{
+    FVector Pos;
+    FVector2D UV;
  
+    FCustomVertex()
+    {
+    }
+ 
+    FCustomVertex(const FVector& VertexPos, const FVector2D& VertexUV)
+        :Pos(VertexPos), UV(VertexUV)
+    {
+    }
+};
+ 
+//declare custom vertex format
+class FMyTestVertexDeclaration : public FRenderResource
+{
+public:
+    FVertexDeclarationRHIRef VertexDeclarationRHI;
+ 
+    virtual void InitRHI() override
+    {
+        FVertexDeclarationElementList Elements;
+        uint16 Stride = sizeof(FCustomVertex);
+ 
+        //Pos-float3
+        Elements.Add(FVertexElement(0, STRUCT_OFFSET(FCustomVertex, Pos), VET_Float3, 0, Stride));
+ 
+        //UV-float2
+        Elements.Add(FVertexElement(0, STRUCT_OFFSET(FCustomVertex, UV), VET_Float2, 1, Stride));
+        VertexDeclarationRHI = PipelineStateCache::GetOrCreateVertexDeclaration(Elements);
+    }
+ 
+    virtual void ReleaseRHI() override
+    {
+        VertexDeclarationRHI.SafeRelease();
+    }
+};
+ 
+TGlobalResource<FMyTestVertexDeclaration> GCustomVertexDeclaration;
+
+
 static void DrawTestShaderRenderTarget_RenderThread(  
     FRHICommandListImmediate& RHICmdList,   
     FTextureRenderTargetResource* OutputRenderTargetResource,  
     ERHIFeatureLevel::Type FeatureLevel,
-    FTexture2DRHIRef InTexture,
+    FTexture* InTextureResource,
     FName TextureRenderTargetName,  
     FLinearColor MyColor  
 )  
 {
-    check(IsInRenderingThread());  
+   check(IsInRenderingThread());
  
-#if WANTS_DRAW_MESH_EVENTS  
-    FString EventName;  
-    TextureRenderTargetName.ToString(EventName);  
-    SCOPED_DRAW_EVENTF(RHICmdList, SceneCapture, TEXT("ShaderTest"));  
-#else  
-    SCOPED_DRAW_EVENT(RHICmdList, DrawUVDisplacementToRenderTarget_RenderThread);  
-#endif  
-
-    // Set the graphic pipeline state.  
-    FGraphicsPipelineStateInitializer GraphicsPSOInit;  
-    RHICmdList.ApplyCachedRenderTargets(GraphicsPSOInit);  
-    
-    //设置渲染目标  
-    OutputRenderTargetResource->UpdateResources(RHICmdList);
-    FRHITexture2D* RenderTargetTexture = OutputRenderTargetResource->GetRenderTargetTexture();
-    FTextureRHIRef TextureRHI = OutputRenderTargetResource->TextureRHI;
-    RHICmdList.TransitionResource(EResourceTransitionAccess::EWritable, RenderTargetTexture);
-    FRHIRenderPassInfo RPInfo(RenderTargetTexture, ERenderTargetActions::DontLoad_Store, RenderTargetTexture);
-    RHICmdList.BeginRenderPass(RPInfo, TEXT("shaderTest"));
-    
-    //设置视口  
-    //FIntPoint DrawTargetResolution(OutputRenderTargetResource->GetSizeX(), OutputRenderTargetResource->GetSizeY());  
-    //RHICmdList.SetViewport(0, 0, 0.0f, DrawTargetResolution.X, DrawTargetResolution.Y, 1.0f);  
-    
-    FGlobalShaderMap* ShaderMap = GetGlobalShaderMap(FeatureLevel);  
-    TShaderMapRef<FShaderTestVS> VertexShader(ShaderMap);  
-    TShaderMapRef<FShaderTestPS> PixelShader(ShaderMap);  
-    
-    
-   
-    GraphicsPSOInit.DepthStencilState = TStaticDepthStencilState<false, CF_Always>::GetRHI();  
-    GraphicsPSOInit.BlendState = TStaticBlendState<>::GetRHI();  
-    GraphicsPSOInit.RasterizerState = TStaticRasterizerState<>::GetRHI();  
-    GraphicsPSOInit.PrimitiveType = PT_TriangleList;  
-    GraphicsPSOInit.BoundShaderState.VertexDeclarationRHI = GetVertexDeclarationFVector4();  
-    GraphicsPSOInit.BoundShaderState.VertexShaderRHI =   VertexShader.GetVertexShader();
-    GraphicsPSOInit.BoundShaderState.PixelShaderRHI = PixelShader.GetPixelShader();  
-    SetGraphicsPipelineState(RHICmdList, GraphicsPSOInit);  
-    
-    //RHICmdList.SetViewport(0, 0, 0.0f, DrawTargetResolution.X, DrawTargetResolution.Y, 1.0f);  
-    PixelShader->SetParameters(RHICmdList, MyColor);  
-    
-    // Draw grid.  
-    //uint32 PrimitiveCount = 2;  
-    //RHICmdList.DrawPrimitive(PT_TriangleList, 0, PrimitiveCount, 1);  
-    FVector4 Vertices[4];  
-    Vertices[0].Set(-1.0f, 1.0f, 0, 1.0f);  
-    Vertices[1].Set(1.0f, 1.0f, 0, 1.0f);  
-    Vertices[2].Set(-1.0f, -1.0f, 0, 1.0f);  
-    Vertices[3].Set(1.0f, -1.0f, 0, 1.0f);  
-    static const uint16 Indices[6] =  
-    {  
-        0, 1, 2,  
-        2, 1, 3  
-    };
-    static const uint32 VERTEX_SIZE = sizeof(FVector4) * 4;
-    FRHIResourceCreateInfo CreateInfo;
-    FVertexBufferRHIRef VertexBufferRHI = RHICreateVertexBuffer(VERTEX_SIZE, BUF_Static, CreateInfo);
-    void* VoidPtr = RHILockVertexBuffer(VertexBufferRHI, 0, VERTEX_SIZE, RLM_WriteOnly);
-    FMemory::Memcpy(VoidPtr, (void*)Vertices, VERTEX_SIZE);
-    RHIUnlockVertexBuffer(VertexBufferRHI);
-    
-    
-    
-    // DrawPrimitiveUP(  
-    //     RHICmdList,  
-    //     PT_TriangleList,  
-    //     0,  
-    //     ARRAY_COUNT(Vertices),  
-    //     2,  
-    //     Indices,  
-    //     sizeof(Indices[0]),  
-    //     Vertices,  
-    //     sizeof(Vertices[0])  
-    // );
-    RHICmdList.SetStreamSource(0, VertexBufferRHI, 0);
-    RHICmdList.DrawPrimitive(0, 2, 1);
-    RHICmdList.EndRenderPass();
-
-    // Resolve render target.  
-    // RHICmdList.CopyToResolveTarget(  
-    //     OutputRenderTargetResource->GetRenderTargetTexture(),  
-    //     OutputRenderTargetResource->TextureRHI,  
-    //     false, FResolveParams());  
+	FRHITexture2D* RenderTargetTexture = OutputRenderTargetResource->GetRenderTargetTexture();
+	RHICmdList.Transition(FRHITransitionInfo(RenderTargetTexture, ERHIAccess::SRVMask, ERHIAccess::RTV));
+ 
+	FRHIRenderPassInfo RPInfo(RenderTargetTexture, ERenderTargetActions::DontLoad_Store);
+	RHICmdList.BeginRenderPass(RPInfo, TEXT("Render Test Shader To Texture"));
+	{
+		FIntPoint ViewportSize(OutputRenderTargetResource->GetSizeX(), OutputRenderTargetResource->GetSizeY());
+	
+		// Upate viewport
+		RHICmdList.SetViewport(0, 0, 0.0f, ViewportSize.X, ViewportSize.Y, 1.0f);
+ 
+		// Get Shaders
+		FGlobalShaderMap* GlobalShaderMap = GetGlobalShaderMap(FeatureLevel);
+		TShaderMapRef<FShaderTestVS> VertexShader(GlobalShaderMap);
+		TShaderMapRef<FShaderTestPS> PixelShader(GlobalShaderMap);
+ 
+		// Setup Pipeline state
+		FGraphicsPipelineStateInitializer GraphicsPSOInit;
+		RHICmdList.ApplyCachedRenderTargets(GraphicsPSOInit);
+		GraphicsPSOInit.DepthStencilState = TStaticDepthStencilState<false, CF_Always>::GetRHI();
+		GraphicsPSOInit.BlendState = TStaticBlendState<>::GetRHI();
+		GraphicsPSOInit.RasterizerState = TStaticRasterizerState<>::GetRHI();
+		GraphicsPSOInit.PrimitiveType = PT_TriangleStrip;
+		GraphicsPSOInit.BoundShaderState.VertexDeclarationRHI = GCustomVertexDeclaration.VertexDeclarationRHI;
+		GraphicsPSOInit.BoundShaderState.VertexShaderRHI = VertexShader.GetVertexShader();
+		GraphicsPSOInit.BoundShaderState.PixelShaderRHI = PixelShader.GetPixelShader();
+		SetGraphicsPipelineState(RHICmdList, GraphicsPSOInit);
+ 
+		// update shader uniform parameters
+		PixelShader->SetParameters(RHICmdList, InTextureResource, MyColor, 1.0);
+ 
+		// Create VertexBuffer and setup
+		static const uint32 VERTEX_SIZE = sizeof(FCustomVertex) * 4;
+		FRHIResourceCreateInfo CreateInfo;
+		FVertexBufferRHIRef VertexBufferRHI = RHICreateVertexBuffer(VERTEX_SIZE, BUF_Static, CreateInfo);
+		void* VoidPtr = RHILockVertexBuffer(VertexBufferRHI, 0, VERTEX_SIZE, RLM_WriteOnly);
+ 
+		FCustomVertex Vertices[4];
+		Vertices[0] = FCustomVertex(FVector(-1.0f, 1.0f, 0.0f), FVector2D(0.0f, 0.0f));
+		Vertices[1] = FCustomVertex(FVector(1.0f, 1.0f, 0), FVector2D(1.0f, 0.0f));
+		Vertices[2] = FCustomVertex(FVector(-1.0f, -1.0f, 0), FVector2D(0.0f, 1.0f));
+		Vertices[3] = FCustomVertex(FVector(1.0f, -1.0f, 0), FVector2D(1.0f, 1.0f));
+		FMemory::Memcpy(VoidPtr, (void*)Vertices, VERTEX_SIZE);
+		RHIUnlockVertexBuffer(VertexBufferRHI);
+ 
+		RHICmdList.SetStreamSource(0, VertexBufferRHI, 0);
+		RHICmdList.DrawPrimitive(0, 2, 1);
+	}
+	RHICmdList.EndRenderPass();
+ 
+	RHICmdList.Transition(FRHITransitionInfo(RenderTargetTexture, ERHIAccess::RTV, ERHIAccess::SRVMask));
 }  
  
 void UTestShaderBlueprintLibrary::DrawTestShaderRenderTarget(  
@@ -239,18 +226,24 @@ void UTestShaderBlueprintLibrary::DrawTestShaderRenderTarget(
     if (!OutputRenderTarget)  
     {  
         return;  
-    }  
-    OutputRenderTarget->UpdateResourceImmediate(true);
-    FTextureRenderTargetResource* TextureRenderTargetResource = OutputRenderTarget->GameThread_GetRenderTargetResource();  
+    }
+
+    if (MyTexture == nullptr)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("MyTexture Is NULL"));
+        return;
+    }
+    FTextureRenderTargetResource* TextureRenderTargetResource = OutputRenderTarget->GameThread_GetRenderTargetResource();
+    FTexture* TextureSource = MyTexture->Resource;
     UWorld* World = Ac->GetWorld();  
     ERHIFeatureLevel::Type FeatureLevel = World->Scene->GetFeatureLevel();  
     FName TextureRenderTargetName = OutputRenderTarget->GetFName();
     // FTextureReferenceRHIRef TextureRHI = MyTexture->TextureReference.TextureReferenceRHI;
 
     ENQUEUE_RENDER_COMMAND(CaptureCommand)(  
-        [TextureRenderTargetResource, FeatureLevel, MyColor, TextureRenderTargetName](FRHICommandListImmediate& RHICmdList)  
+        [TextureRenderTargetResource,TextureSource, FeatureLevel, MyColor, TextureRenderTargetName](FRHICommandListImmediate& RHICmdList)  
         {
-            DrawTestShaderRenderTarget_RenderThread(RHICmdList,TextureRenderTargetResource, FeatureLevel, nullptr, TextureRenderTargetName, MyColor);  
+            DrawTestShaderRenderTarget_RenderThread(RHICmdList,TextureRenderTargetResource, FeatureLevel, TextureSource, TextureRenderTargetName, MyColor);  
         }  
     );  
  
